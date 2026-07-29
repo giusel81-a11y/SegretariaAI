68ab1d3b5673fae1354aa3dc05f1369a7bada7d0
giusel81-a11y <giusel81@gmail.com>
2026-07-29 18:16:52 +0200

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,3 +1,25 @@
+from database import conn
+
+
+def add_note(text):
+
+    conn.execute(
+        "INSERT INTO notes(text) VALUES(?)",
+        (text,)
+    )
+
+    conn.commit()
+
+
+def get_notes():
+
+    rows = conn.execute(
+        "SELECT text FROM notes"
+    )
+
+    return [r[0] for r in rows]
+
+
 def complete_note(note_id):
 
     conn.execute(
