01eb38bf7f67b712dac8280efa296931f9f91be3
giusel81-a11y <giusel81@gmail.com>
2026-07-29 18:08:19 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,21 +1,8 @@
-import sqlite3
-import os
-
-os.makedirs("data", exist_ok=True)
-
-conn = sqlite3.connect(
-    "data/database.db",
-    check_same_thread=False
-)
-
-cursor = conn.cursor()
-
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS notes(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     text TEXT,
+    completed INTEGER DEFAULT 0,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 """)
-
-conn.commit()
